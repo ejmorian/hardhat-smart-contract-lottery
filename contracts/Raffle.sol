3863afa4093bd35051bdad25bd3b1fc2af744c3a
ejmorian <ejmorian@gmail.com>
2023-04-12 22:53:39 +0800

    add enterRaffle function and deploy script

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,7 +1,43 @@
 //Raffle
 // Enter the lottery (1Eth per entry)
 // Pick a random winner
-// Winner selection to be selected per week 
+// Winner selection to be selected per week
 // use a chainlink oracle for randomness
 // automated execution (chainlink keepers)
 
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+error enterRaffle__insufficientAmount();
+
+contract Raffle {
+    /* State Variables */
+    uint256 private immutable i_entranceFee;
+    address payable[] public s_participants;
+
+    /* Events */
+    event RaffleEnter(address indexed participant);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() external payable {
+        // require(msg.value > i_entranceFee)
+        if (msg.value < i_entranceFee) {
+            revert enterRaffle__insufficientAmount();
+        }
+
+        s_participants.push(payable(msg.sender));
+
+        emit RaffleEnter(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {}
+
+    function getParticipant(uint256 index) public view returns (address) {
+        return s_participants[index];
+    }
+
+    // function pickRandomWinner() external {}
+}
