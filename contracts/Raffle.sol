903798ae8810cf51138d6d3586d2a027d96607c0
ejmorian <ejmorian@gmail.com>
2023-04-12 21:22:34 +0800

    initial commit

---
+++ contracts/Raffle.sol
@@ -0,0 +1,7 @@
+//Raffle
+// Enter the lottery (1Eth per entry)
+// Pick a random winner
+// Winner selection to be selected per week 
+// use a chainlink oracle for randomness
+// automated execution (chainlink keepers)
+
